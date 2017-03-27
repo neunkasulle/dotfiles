961d7c8412b55479824d9b198e4bbda3e63b6896
Jan Zenkner <jzenkner@riverbed.com>
2017-03-27 11:18:05 +0200

    [dotfile] add vimrc

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 ln Xdefaults ~/.Xdefaults
 ln i3config  ~/.config/i3/config
+git clone http://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+ln vimrc ~/.vimrc
+vim +PluginInstall +qall
 
